27af107a34d97946db03ee56b2136b7cd0f42a14
Nebojsa Horvat <horvatnebojsa@yahoo.com>
2018-05-03 00:36:46 +0200

    new chat nods will now tell master node to add them

--- ChatEjb/ejbModule/cluster/ClusterManager.java
+++ ChatEjb/ejbModule/cluster/ClusterManager.java
@@ -6,11 +6,21 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.ConcurrencyManagement;
 import javax.ejb.ConcurrencyManagementType;
+import javax.ejb.EJB;
 import javax.ejb.Lock;
 import javax.ejb.LockType;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+
+import config.PropertiesSupplierLocal;
+import jms_messages.UserAuthResMsg;
 import model.Host;
 
 @ConcurrencyManagement(ConcurrencyManagementType.CONTAINER)
@@ -19,11 +29,30 @@ import model.Host;
 @Singleton
 public class ClusterManager implements ClusterManagerLocal{
 
+	@EJB
+	private PropertiesSupplierLocal prop;
+	
 	private List<Host> activeHosts;
+	
 	@PostConstruct
 	private void init () {
 		activeHosts = new ArrayList<>();
-	   
+		
+		String isMaster = prop.getProperty("IS_MASTER");
+		// TODO PROMENITI OVO NA FALSE POSLE TESTIRANJA
+		if( isMaster.equals("true"))// pre isMaster... NE treba da ide !
+			return ;
+		// Napravim objekat Host koji predtavlja ovaj node koji salje masteru da se registruje
+		Host host = new Host(
+				prop.getProperty("LOCATION"),
+				prop.getProperty("NAME_OF_NODE"),
+				Integer.parseInt(prop.getProperty("PORT")) );
+		// Sad trazim on mastera da me registruje mendju postojece nodove i ubaci u evidenciju
+		ResteasyClient client = new ResteasyClientBuilder().build();
+		String targetString = "http://"+prop.getProperty("MASTER_LOCATION")+":"+prop.getProperty("MASTER_PORT")+"/ChatWeb/rest/cluster/addHost";
+		ResteasyWebTarget target = client.target(targetString);
+		Response response = target.request().post(Entity.entity(host, MediaType.APPLICATION_JSON));
+		
 	}
 	
 	@Lock(LockType.WRITE)
--- ChatEjb/ejbModule/cluster/ClusterManagerLocal.java
+++ ChatEjb/ejbModule/cluster/ClusterManagerLocal.java
@@ -2,7 +2,13 @@ package cluster;
 
 import javax.ejb.Local;
 
+import model.Host;
+
 @Local
 public interface ClusterManagerLocal {
+
+	public void addHostToActiveList(Host host);
+
+	public void removeHostFromActiveList(Host host);
 
 }
