9dce65ec6e6727136882b3605b38674797ea50eb
Nebojsa Horvat <horvatnebojsa@yahoo.com>
2018-05-04 05:52:07 +0200

    groups change front

--- UserEjb/ejbModule/beans/Test.java
+++ UserEjb/ejbModule/beans/Test.java
@@ -41,13 +41,13 @@ public class Test {
 		u1 = userService.add(u1);
 		userService.add(u2);
 		
-//		ArrayList<String> users = new ArrayList<>();
-//		users.add(u1.getUsername());
-//		users.add(u2.getUsername());
-//		
-//		Chat chat1 = chatMessageService.creteChat(new Chat(users, null, (long)1525024791));
-//		chatMessageService.saveMessage(chat1.getId(), new Message("test", (long)1525024791, "first"));
-//		chatMessageService.saveMessage(chat1.getId(), new Message("proba", (long)1525024791, "second"));
-//		chatMessageService.saveMessage(chat1.getId(), new Message("test", (long)1525024791, "third"));
+		ArrayList<String> users = new ArrayList<>();
+		users.add(u1.getUsername());
+		users.add(u2.getUsername());
+		
+		Chat chat1 = chatMessageService.creteChat(new Chat(users, null, (long)1525024791));
+		chatMessageService.saveMessage(chat1.getId(), new Message("test", (long)1525024791, "first"));
+		chatMessageService.saveMessage(chat1.getId(), new Message("proba", (long)1525024791, "second"));
+		chatMessageService.saveMessage(chat1.getId(), new Message("test", (long)1525024791, "third"));
 	}
 }
